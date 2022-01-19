a5fcf5e5c4341fc844021c46e02f35e9d48aa770
Kanishk Jamgaonkar <code.kanishk@gmail.com>
2022-01-20 00:52:50 +0530

    Update settings.py

--- securesite/settings.py
+++ securesite/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
